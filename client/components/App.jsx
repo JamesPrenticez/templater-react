b75e6dde704e14020f724978536d9d0ad0392060
James-Prentice <Jamesprenticez@gmail.com>
2020-10-18 12:26:29 +1300

    Added navigation for view

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,13 +5,18 @@ import JobList from './JobList'
 import AddJob from './AddJob'
 import JobContract from './JobContract'
 
+
 const App = () => {
   return (
+<>
+    <div className='main'>
     <Router>
       <Route exact path='/' component={JobList} />
       <Route path='/add' component={AddJob} />
       <Route path='/view' component={JobContract} />
     </Router>
+    </div>
+</>
   )
 }
 
--- client/components/JobContract.jsx
+++ client/components/JobContract.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Nav from './Nav'
+
 export default class JobContract extends React.Component {
 render(){
     const {job, dispatch} = this.props
 
         return(
             <>
-            <Link to={`/`}>
-                <h4>Home</h4>
-            </Link>
+        <Nav></Nav>
+
+        <Link to={`/`}>
+            <h4>Home</h4>
+        </Link>
+      
+
             </>
         )
     }
