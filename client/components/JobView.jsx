2cdbfec28ee19ecb54f897d2cae13a8d90bc66e3
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-02 17:03:04 +1300

    Need to fix seeds

--- client/components/JobView.jsx
+++ client/components/JobView.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 
 class JobView extends React.Component {
@@ -12,39 +12,44 @@ class JobView extends React.Component {
             <>
 
                 <div className="jobContainer">
-                    <div className="formHeader">
-                        <h4>Job Details</h4>
-                    </div>
+                        <div className="formHeader">
+                            <h4>Job Details</h4>
+                        </div>
 
-                    <div id="jobDetails">
-                        <h4>Date Created</h4>
-                        <p>{this.props.jobs[jobName].jobName}</p>
- 
-                        <h4>Job Name</h4>
-                        <p>{this.props.jobs[jobName].jobName}</p>
 
-                        <h4>Client Name</h4>
-                        <p>{this.props.jobs[jobName].clientName}</p>
- 
-                        <h4>Site Address</h4>
-                        <p>{this.props.jobs[jobName].siteAddress}</p>
 
-                        <h4>Suburb</h4>
-                        <p>{this.props.jobs[jobName].suburb}</p>
+                        <div className="jobDetails">
+                        
+                            <h4>Date Created</h4>
+                            <p>{this.props.jobs[jobName].jobName}</p>
 
-                        <h4>Lot Number</h4>
-                        <p>{this.props.jobs[jobName].lotNumber}</p>
+                            <h4>Job Name</h4>
+                            <p>{this.props.jobs[jobName].jobName}</p>
 
-                        <h4>Job Number</h4>
-                        <p>{this.props.jobs[jobName].jobNumber}</p>
+                            <h4>Client Name</h4>
+                            <p>{this.props.jobs[jobName].clientName}</p>
 
-                        <h4>Sales Person</h4>
-                        <p>{this.props.jobs[jobName].salesPerson}</p>
+                            <h4>Site Address</h4>
+                            <p>{this.props.jobs[jobName].siteAddress}</p>
 
-                        <h4>GFA</h4>
-                        <p>{this.props.jobs[jobName].salesPerson}</p>
+                            <h4>Suburb</h4>
+                            <p>{this.props.jobs[jobName].suburb}</p>
 
-                    </div>
+                            <h4>Lot Number</h4>
+                            <p>{this.props.jobs[jobName].lotNumber}</p>
+
+                            <h4>Job Number</h4>
+                            <p>{this.props.jobs[jobName].jobNumber}</p>
+
+                            <h4>Sales Person</h4>
+                            <p>{this.props.jobs[jobName].salesPerson}</p>
+
+                            <h4>GFA</h4>
+                            <p>{this.props.jobs[jobName].salesPerson}</p>
+
+                            <img style={{ backgroundImage: `url(${this.props.jobs.image_cover})` }}></img>
+
+                        </div>
 
                 </div>
             </>
--- seeds/1-jobs.js
+++ seeds/1-jobs.js
@@ -6,14 +6,17 @@ exports.seed = (knex, Promise) => {
       jobName: 'Alexandra',
       clientName: 'John Doe',
       siteAddress: '01 Fake Street',
-      suburb: 'Southern Series',
+      collection: 'Southern Series',
       lotNumber: 'Lot 01',
       jobNumber: 'SS01',
       gfa: "170.69m2",
-      salesPerson: 'Standard Pricing'
+      salesPerson: 'Standard Pricing',
+      image_cover: './img/houses/alexandra/alexandra.png'
     },
   ])
 }
+
+
 
 
 
