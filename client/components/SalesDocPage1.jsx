2266cc3b664ca049cd2171c08cdfa13e1569e7da
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-15 16:21:59 +1300

    Main Page Complete

--- client/components/SalesDocPage1.jsx
+++ client/components/SalesDocPage1.jsx
@@ -13,7 +13,7 @@ render(){
         return(
             <>
                 <div className='pageHeader'></div>
-                    <h1>Schedule Of Materials & Works</h1>
+                    <h3 style={{textAlign: 'center'}}>Schedule Of Materials & Works</h3>
 
                     <table id='mainPageTable'>
                             <tbody>
@@ -21,49 +21,105 @@ render(){
                                     <td className='a'>NEW HOME OWNER:</td>
                                     <td className='b'>{jobs[i].jobName}</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>JOB ADDRESS:</td>
                                     <td className='b'>{jobs[i].jobNumber}</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>SITE ADDRESS:</td>
                                     <td className='b'>{jobs[i].siteAddress}</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>DATE:</td>
                                     <td className='b'>{moment().format('D MMM, YYYY')}</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>HOUSE/TYPE:</td>
                                     <td className='b'>DESIGN/BUILD</td>{/*Need to add this into the database*/}
                                 </tr>
 
-                                <tr>{/*Empty Line*/}</tr>
+                                <tr className='blankRow'>
+                                    {/*Empty Line*/}
+                                    <td></td>
+                                    <td></td>
+                                </tr>
 
                                 <tr>
                                     <td className='a'>RETAIL PRICE</td>
-                                    <td className='b'>{jobs[i].cost}</td>
+                                    <td className='b'>{jobs[i].cost} (Including GST)</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>CONTRACT PRICE</td>
-                                    <td className='b'>{jobs[i].cost}</td>
+                                    <td className='b'>{jobs[i].cost} (Including GST)</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>LESS DEPOSIT</td>
-                                    <td className='b'>$5000</td>
+                                    <td className='b'>$5000 (Including GST)</td>
                                 </tr>
+
                                 <tr>
                                     <td className='a'>BALANCE</td>
-                                    <td className='b'>{jobs[i].cost} - 5000 </td>
+                                    <td className='b'>{jobs[i].cost} (Including GST)</td>{/*Need to preform some maths on this*/}
+                                </tr>
+
+                                <tr className='blankRow'>
+                                    {/*Empty Line*/}
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td className='a'>GROUND FLOOR AREA (GFA):</td>
+                                    <td className='b'>{jobs[i].gfa}</td>
+                                </tr>
+                                <tr>
+                                    <td className='a'>HOME CONSULTANT:</td>
+                                    <td className='b'>{jobs[i].salesPerson}</td>
+                                </tr>
+
+                                <tr className='blankRow'>
+                                    {/*Empty Line*/}
+                                    <td></td>
+                                    <td></td>
                                 </tr>
 
                             </tbody>
                         </table>
 
+                <div className='clientAppoval'>
+                    <h6>CLIENT APPROVAL</h6>
+                    <ol>
+                        <li>Before you read this contract, it is important that you understand that this document is the basis for building your new home. All previous discussions that you have had with the consultant about various products etc. are now irrelevant as this document represents what you have finally agreed on</li>
+                        <li>To ensure that you acknowledge all the aspects of this schedule, we will tick each clause upon your confirmation that you fully understand and for any reason you do not – please ask your consultant to explain</li>
+                    </ol>
+                </div>
+
+                <div className='contractName'>
+                    <h6>CONTRACT NAMES</h6>
+                    <ol>
+                        <li>Before you read this contract, it is important that you understand that this document is the basis for building your new home. All previous discussions that you have had with the consultant about various products etc. are now irrelevant as this document represents what you have finally agreed on</li>
+                        <li>To ensure that you acknowledge all the aspects of this schedule, we will tick each clause upon your confirmation that you fully understand and for any reason you do not – please ask your consultant to explain</li>
+                    </ol>
+                </div>
+
+                <div className='certificateOfTitle'>
+                    <h6>CERTIFICATED OF TITLE</h6>
+                    <ol>
+                        <li>Before you read this contract, it is important that you understand that this document is the basis for building your new home. All previous discussions that you have had with the consultant about various products etc. are now irrelevant as this document represents what you have finally agreed on</li>
+                    </ol>
+                </div>
 
                 <div className='pageFooter'>
                     <div id='hozLine'></div>
+                    Initial for Customer:
                     Page 1
+                    Initial for Jennian Homes:
                 </div>
             </>
         )
