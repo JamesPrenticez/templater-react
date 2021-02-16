a0c7184cc04fd52f986d890b1d7ff11634e79dba
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-16 13:21:21 +1300

    Page 7 - data

--- client/components/SalesDocPage7.jsx
+++ client/components/SalesDocPage7.jsx
@@ -15,33 +15,50 @@ render(){
             <>
                 <div className='pageHeader'></div>
 
-                <table id='mainPageTable'>
-                        <tbody>
-                                <tr>
-                                <td className='a'>HOME CONSULTANT:</td>
-                                <td className='b'>{jobs[i].salesPerson}</td>
-                            </tr>
-
-                            <tr className='blankRow'>
-                                {/*Empty Line*/}
-                                <td></td>
-                                <td></td>
-                            </tr>
-
-                        </tbody>
-                </table>
-
-                <div className='clientAppoval'>
-                    <h6>x</h6>
+                <div className='interiorDoors'>
+                    <h6>INTERIOR DOORS</h6>
                     <ol>
-                        <li></li>
+                        <li>All doors shall be hollow core MDF flush panel paint grade 1980mm high</li>
+                        <li>Door jambs shall be HAG 11 paint grade</li>
+                        <li>Door jambs shall be pine clears paint grade to the ensuite and bathroom </li>
+                        <li>All the interior door handles shall be Windsor Corbel lever handles</li>
+                        <li>Windsor 9034 privacy locks will be fitted to the WC, ensuite and bathroom only </li>
+                        <li>Skirting mounted Windsor 5240 door stops to all doors</li>
+                        <li>Finish to be Brushed Nickel</li>
                     </ol>
+                    <div className='salesImages'></div>
+                    <div className='salesImages'></div>
+                    <div className='salesImages'></div>
+                    <div className='salesImages'></div>
+                </div>
+
+                <div className='wardrobes'>
+                    <h6>WARDROBES</h6>
+                    <ol>
+                        <li>Wardrobe joinery (value $2,524.25)</li>
+                        <li>Master bedroom – Melamine shelves, full and double hang walk in wardrobe system 1800mm x 1620mm</li>
+                        <li>Bedroom 2 & 3 – Melamine single shelf and pole system 1900mm</li>
+                        <li>Bedroom 4 – Melamine single shelf and pole system 1400mm</li>
+                        <li>Laundry – Melamine set of four shelves 900mm wide x 450mm deep</li>
+                    </ol>
+                    <div className='salesImages'></div>
+                    <div className='salesImages'></div>
+                    <div className='salesImages'></div>
+                </div>
+
+                <div className='ceilingAccess'>
+                    <h6>CEILING ACCESS & ATTIC STORAGE</h6>
+                    <ol>
+                        <li>Shall include a standard ceiling access panel located in the garage to provide access to the roof space</li>
+                        <li>No allowance has been made for an attic storage space</li>
+                    </ol>
+                    <div className='salesImages'></div>
                 </div>
 
                 <div className='pageFooter'>
                     <div id='hozLine'></div>
                     Initial for Customer:
-                    Page 3
+                    Page 7
                     Initial for Jennian Homes:
                 </div>
             </>
