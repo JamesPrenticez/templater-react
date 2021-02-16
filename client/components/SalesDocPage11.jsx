d4988706b0f395095362625c4f35f02a358b4c21
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-16 14:26:59 +1300

    Page 11 - data

--- client/components/SalesDocPage11.jsx
+++ client/components/SalesDocPage11.jsx
@@ -15,20 +15,120 @@ render(){
             <>
                 <div className='pageHeader'></div>
 
-                <div className=''>
-                    <h6></h6>
-                    <ol>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ol>
-                    <div className='salesImages'></div>
-                </div>
+                {/*--------- BATHROOM ---------*/}
+                <table id='bathroomTable'>
+                    <tbody>
+                        <tr>
+                            <td className='c'>Shower:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Vanity:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Bath:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Shower Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Shower Slide:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Basin Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Bath Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Bath Spout:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                    </tbody>
+                </table>
 
+                {/*--------- WC ---------*/}
+                <table id='wcTable'>
+                    <tbody>
+                        <tr>
+                            <td className='c'>Toilet:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Toilet Roll Holder:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                {/*--------- ENSUITE ---------*/}
+                <table id='wcTable'>
+                    <tbody>
+                        <tr>
+                            <td className='c'>Shower:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Vanity:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Shower Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Shower Slide:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Basin Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Bath Mixer:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Toilet:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                        <tr>
+                            <td className='c'>Toilet Roll Holder:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                {/*--------- LAUNDRY ---------*/}
+                <table id='wcTable'>
+                    <tbody>
+                        <tr>
+                            <td className='c'>Tub:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                {/*--------- KITCHEN ---------*/}
+                <table id='kitchenTable'>
+                    <tbody>
+                        <tr>
+                            <td className='c'>Tub:</td>
+                            <td className='d'>{}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
                 <div className='pageFooter'>
                     <div id='hozLine'></div>
                     Initial for Customer:
-                    Page 8  
+                    Page 11  
                     Initial for Jennian Homes:
                 </div>
             </>
