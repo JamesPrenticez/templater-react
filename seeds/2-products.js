fdcb45656dbb2202bc02aec53888131100e8acae
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-18 09:30:28 +1300

    Image Dump

--- seeds/2-products.js
+++ seeds/2-products.js
@@ -2,11 +2,10 @@ exports.seed = (knex, Promise) => {
   return knex('products').insert([
     {
       id: 0,
-      type: 'Provisional Sum',
+      category: 'Provisional Sum',
       name: 'Building Consent',
-      code: '123',
       price: '$0,000.00',
-      url: 'img/test',
+      url: 'img/products',
     }
   ])
 }
